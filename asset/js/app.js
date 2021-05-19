23e487706be4815526c4bb3ec31780cc7a6731b5
domingo79 <andria140779@yahoo.it>
2021-05-19 15:33:32 +0200

    aggiungi-rimuovi

--- asset/js/app.js
+++ asset/js/app.js
@@ -1,12 +1,33 @@
 const app = new Vue({
     el: '#app',
     data: {
+        nuovaLista: '',
         listaSpesa: [
             'latte',
             'pane',
             'uova',
             'faria'
-        ]
+        ],
     },
-    methods: {}
+    methods: {
+        aggiungi() {
+            if (!this.listaSpesa.includes(this.nuovaLista)) {
+
+                this.listaSpesa.push(this.nuovaLista)
+            } else if (this.nuovaLista.length <= 0) {
+                console.log('non aggiunto nulla');
+            }
+            else {
+                alert('prodotto già inserito')
+                //console.log('gia inserito');
+            }
+            this.nuovaLista = ''
+        },
+
+        rimuovi(index) {
+            console.log('rimozione di: ' + index);
+            this.listaSpesa.splice(index, 1)
+        }
+
+    }
 })
